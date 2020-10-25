212fb179eb236674f8d80b078308bcbe2616212c
qinbingbing <qinbingbing@bytedance.com>
2020-10-25 15:48:21 +0800

    has-a: private inheritance

---
+++ char14_02/student.cpp
@@ -0,0 +1,74 @@
+// student.cpp -- Student class using private inheritance
+#include "studenti.h"
+using std::ostream;
+using std::endl;
+using std::istream;
+using std::string;
+
+// public methods
+double Student::Average() const
+{
+    if (ArrayDb::size() > 0)
+        return ArrayDb::sum() / ArrayDb::size();
+    else
+        return 0;
+}
+
+const std::string & Student::Name() const
+{
+    return (const string &) *this;
+}
+
+double & Student::operator[](int i)
+{
+    return ArrayDb::operator[](i);  // use ArrayDb::operator[]() 
+}
+
+double Student::operator[](int i) const
+{
+    return ArrayDb::operator[](i);
+}
+
+// private method
+std::ostream & Student::arr_out(std::ostream & os) const
+{
+    int i; 
+    int lim = ArrayDb::size();
+    if (lim > 0)
+    {
+        for (i = 0; i < lim; ++i)
+        {
+            os << ArrayDb::operator[](i) << " ";
+            if (i % 5 == 4)
+                os << endl;
+        }
+        if (i % 5 != 0)
+            os << endl;
+    }
+    else
+        os << " empty array ";
+    return os;
+}
+
+// friends
+// use string version of operator>>()
+istream & operator>>(istream & is, Student & stu)
+{
+    is >> (string &) stu;
+    return is;
+}
+
+// use string friend getline(istream &, const string &)
+istream & getline(istream & is, Student & stu)
+{
+    getline(is, (string &) stu);
+    return is;
+}
+
+// use string version of operator<<()
+ostream & operator<<(ostream & os, const Student & stu)
+{
+    os << "Scores for " << (const string &) stu << ":\n";
+    stu.arr_out(os);  // use private method for scores
+    return os;
+}
